e35b4d0517ee861b1c54e0693a55ef8e682204be
tobudim <tobudim@mdp>
2020-08-27 02:59:27 +0200

    add homepage and blog architecture

---
+++ pages/blog/index.js
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import Date from "../../components/date";
+import Layout, { siteTitle } from "../../components/layout";
+import { getSortedPostsData } from "../../lib/posts";
+import utilStyles from "../../styles/utils.module.css";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return { props: { allPostsData } };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Indépendance numérique, programmation et partage.</p>
+        <small className={utilStyles.lightText}>
+          Ce blog, fièrement, ni ne vous traque, ni ne transmet votre visite et
+          ses détails aux GAFAM.
+        </small>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Articles</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/blog/[id]" as={`/blog/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
