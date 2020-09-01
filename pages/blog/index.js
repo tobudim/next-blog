c28a896d9f7ce676ae81db800088b41fb5dd992b
tobudim <tobudim@mdp>
2020-09-01 19:03:50 +0200

    comment out blog/index.js and update lib/posts.js to return posts sorted by year if boolean

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,52 +1,56 @@
-import Head from "next/head";
-import Link from "next/link";
-
-import Date from "../../components/date";
-import Layout, { siteTitle } from "../../components/layout";
+// import Head from "next/head";
+// import Link from "next/link";
+//
+// import Date from "../../components/date";
+// import Layout, { siteTitle } from "../../components/layout";
 import { getSortedPostsData } from "../../lib/posts";
-import utilStyles from "../../styles/utils.module.css";
+// import utilStyles from "../../styles/utils.module.css";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return { props: { allPostsData } };
+  const yearSortedPosts = getSortedPostsData(true);
+  return { props: { yearSortedPosts } };
 }
 
-export default function Home({ allPostsData }) {
-  return (
-    <Layout blogIndex>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <h2>Blog de Dimitri Bourreau</h2>
-        <p>Indépendance numérique, programmation et partage.</p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Articles</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href="/blog/[id]" as={`/blog/${id}`}>
-                <a>
-                  {title}
-                  <small
-                    className={utilStyles.lightText}
-                    style={{ marginLeft: "15px" }}
-                  >
-                    <Date dateString={date} />
-                  </small>
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <hr className={utilStyles.divider} />
-        <small className={utilStyles.lightText}>
-          Ce blog, fièrement, ni ne vous traque, ni ne transmet votre visite et
-          ses détails aux GAFAM.
-        </small>
-      </section>
-    </Layout>
-  );
+// export default function Home({ yearSortedPosts }) {
+export default function Home() {
+  return <h1>OK</h1>;
+  // return (
+  // <Layout blogIndex>
+  // <Head>
+  // <title>{siteTitle}</title>
+  // </Head>
+  // <section className={utilStyles.headingMd}>
+  // <h2>Blog de Dimitri Bourreau</h2>
+  // <p>Indépendance numérique, programmation et partage.</p>
+  // </section>
+  // <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+  // <h2 className={utilStyles.headingLg}>Articles</h2>
+  // { Object.keys(yearSortedPosts).forEach(year => (
+  // <h3>{year}</h3>
+  // // {yearSortedPosts[year].forEach({id, date, title} => (
+  // <ul className={utilStyles.list}>
+  // <li className={utilStyles.listItem} key={id}>
+  // // // <Link href="/blog/[id]" as={`/blog/${id}`}>
+  // <a>
+  // {title}
+  // <small
+  // className={utilStyles.lightText}
+  // style={{ marginLeft: "15px" }}
+  // >
+  // <Date dateString={date} />
+  // </small>
+  // </a>
+  // </Link>
+  // </li>
+  // </ul>
+  // ))};
+  // )) }
+  // <hr className={utilStyles.divider} />
+  // <small className={utilStyles.lightText}>
+  // Ce blog, fièrement, ni ne vous traque, ni ne transmet votre visite et
+  // ses détails aux GAFAM.
+  // </small>
+  // </section>
+  // </Layout>
+  // );
 }
