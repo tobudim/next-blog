a9d1ab398564eec1366dff1ea997351d89e80db2
tobudim <tobudim@mdp>
2020-09-04 08:00:12 +0200

    update 404

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,20 @@
+import Head from "next/head";
+import Link from "next/link";
+
+import Layout, { siteTitle } from "../components/layout";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <Layout>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <h1>404 - Page inconnue</h1>
+      <Link href="/">
+        <a>
+          <h3>Revenir à l&apos;accueil</h3>
+        </a>
+      </Link>
+    </Layout>
+  );
 }
