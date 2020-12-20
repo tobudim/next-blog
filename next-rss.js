432f28001532a6903b094737aa129a4b601de8b5
tobudim <tobudim@protonmail.com>
2020-12-20 16:01:36 +0000

    add: next-rss package

---
+++ next-rss.js
@@ -0,0 +1,9 @@
+module.exports = {
+  siteTitle: "Dimitri Bourreau : Développeur",
+  siteDescription: "Dimitri Bourreau : Développeur",
+  siteLanguage: "fr",
+  siteCopyright: "Dimitri Bourreau",
+  siteUrl: "https://dimitribourreau.me",
+  outDir: "public",
+  postsDir: "posts",
+};
